08364c66c2e9ce648ba98aafa7156ac884b2cc27
Sara Nicole Cabrera <142828975+saraccantillo@users.noreply.github.com>
2024-10-22 21:18:02 -0500

    Delete uliMaps directory

--- uliMaps/user/user.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-user = Blueprint("user", __name__, static_folder="static", template_folder="templates")
-
-@user.route("/")
-def index():
-    return render_template("usuario/index.html")
